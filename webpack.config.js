e03d59c54967ebbe808185c212febb6e07494f6d
aslesarev <aslesarev@htc-cs.ru>
2020-06-08 12:34:49 +0400

    add css, html optimization

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,30 @@ const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserJSPlugin = require('terser-webpack-plugin');
 
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = process.env.NODE_ENV === 'production';
+
+const optimization = () => {
+    const config = {
+        splitChunks: {
+            chunks: 'all'
+        }
+    }
+
+    if(isProd) {
+        config.minimizer = [
+            new OptimizeCSSAssetsPlugin(),
+            new TerserJSPlugin()
+        ]
+    }
+
+    return config;
+};
 
 module.exports = {
     mode: 'development',
@@ -17,23 +40,48 @@ module.exports = {
     resolve: {
         extensions: ['.js']
     },
+    optimization: optimization(),
     plugins: [
         new HtmlWebpackPlugin({
-            template: './src/index.html'
+            template: './src/index.html',
+            minify: {
+                collapseWhitespace: isProd
+            }
         }),
-        new CleanWebpackPlugin()
+        new CleanWebpackPlugin(),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, 'src/favicon.ico'),
+                    to: path.resolve(__dirname, 'build')
+                }
+            ]
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        })
     ],
     devServer: {
-        port: 8080
+        port: 8080,
+        hot: isDev
     },
     module: {
         rules: [
             {
                 test: /\.css$/, 
-                use: ['style-loader','css-loader']
+                use: [
+                        {
+                            loader: MiniCssExtractPlugin.loader,
+                            options: {
+                                hmr: isDev,
+                                reloadAll: true
+                            }
+                        },
+                        'css-loader'
+                ],              
             },
             {
-                test: /\.(png|jpe?g|gif|svg)$/i,
+                test: /\.(png|jpe?g|gif|svg|ico)$/i,
                 loader: 'file-loader',
                 options: {
                     outputPath: 'images',
